2f15f48008742c9a1e973b6bbde43b57f0030a93
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-14 17:11:28 +0530

    Added nav bar

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
 import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 
 function Header(){
 	return (
@@ -10,11 +11,46 @@ function Header(){
 
 		  		<img alt="logo" className="header__logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
 			</Link>
+
 			{/* Search box */}
 			<div className="header__search">
 				<input type="text" className="header__searchInput" />
 				<SearchIcon className="header__searchIcon" />
 			</div>
+
+			{/* 3 links */}
+			<div className='header__nav'>
+				{/* 1st link */}
+					<Link to="/login" className="header__link">
+						<div className="header__option">
+							<span className="header__optionLineOne">Hello Bharath</span>
+							<span className="header__optionLineTwo">Sign In</span>
+						</div>
+					</Link>
+				{/* 2nd link */}
+					<Link to="/" className="header__link">
+						<div className="header__option">
+							<span className="header__optionLineOne">Returns</span>
+							<span className="header__optionLineTwo">& Orders</span>
+						</div>
+					</Link>
+				{/* 3rd link */}
+					<Link to="/" className="header__link">
+						<div className="header__option">
+							<span className="header__optionLineOne">Your</span>
+							<span className="header__optionLineTwo">Prime</span>
+						</div>
+					</Link>
+				{/* 4th link */}
+					<Link to="/checkout" className="header__link">
+						<div className="header__optionBasket">
+							<ShoppingBasketIcon />
+							<span className="header__optionLineTwo header__basketCount">0</span>
+						</div>
+					</Link>
+
+			</div>
+
 		</nav>
 	)
 }
